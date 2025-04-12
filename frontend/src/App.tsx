9ce200ed73b592e3be35c5c78380ff0c348cd7bd
양준서(JoonSeo Yang) <wnstj1704@gmail.com>
2025-04-12 23:28:10 +0900

    [setting] set some folder structure
    - test and practicing react button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,51 @@
 import { useState } from 'react';
 import './App.css';
 
-function MyButton() {
-  return <button className="text-amber-400">I'm a Button</button>;
-}
+// interface formData {
+//   username: string;
+//   email: string;
+//   password: string;
+//   confirmPassword: string;
+// }
+
+// const MyButton = () => {
+//   return <button className="text-amber-400">회원가입</button>;
+// };
+
+const Signup = () => {
+  const [signupVisible, setSignupVisible] = useState<boolean>(false);
+  // const [formData, setFormData] = useState<formData>({
+  //   username: '',
+  //   email: '',
+  //   password: '',
+  //   confirmPassword: '',
+  // });
+
+  return (
+    <>
+      <div>
+        <button
+          className="text-amber-400"
+          onClick={() => setSignupVisible(true)}
+        >
+          회원가입
+        </button>
+      </div>
+      {signupVisible && (
+        <div className="fixed bg-opacity-40 flex-col p-4 items-center justify-center z-50 border-2 border-amber-200">
+          <h3>--회원가입--</h3>
+          <form>
+            <input type="text" placeholder="이름" />
+            <div>
+              <button onClick={() => setSignupVisible(false)}>취소</button>
+              <button>가입하기</button>
+            </div>
+          </form>
+        </div>
+      )}
+    </>
+  );
+};
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,7 +58,8 @@ function App() {
           count is {count}
         </button>
       </div>
-      <MyButton />
+      {/* <MyButton /> */}
+      <Signup />
     </>
   );
 }
